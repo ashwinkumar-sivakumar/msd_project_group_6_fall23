d93b076b16c15c96dffe424e04c07640a748c511
RegallaMahidhar <150110917+RegallaMahidhar@users.noreply.github.com>
2023-11-18 19:07:32 -0800

    Update msd_dimm.sv
    modified $finish condition

--- checkpoint2/rtl/msd_dimm.sv
+++ checkpoint2/rtl/msd_dimm.sv
@@ -1,4 +1,4 @@
-module msd_dimm;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+odule msd_dimm;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
  
   int traceFile,out_file;
   int valuesRead;
@@ -25,8 +25,8 @@ module msd_dimm;
        local_var = {q_ip_oper.pop_front(), q_ip_addr.pop_front()};
        if (debug_en)
           q_mc.push_back(local_var);
-          $display(">>>@time:%t Adding new element to the queue.. %h ... at sim_time = %0d --> q_mc.size = %0d\n",$time, local_var, sim_time, q_mc.size()); 
-          display_q; 
+          $display(">>>@time:%t Adding new element to the queue.. %h ... at sim_time = %0d --> q_mc.size = %0d\n",$time, local_var, sim_time, q_mc.size());
+          display_q;
   endtask
  
   task output_command(input[37:0] q_out_temp);
@@ -37,7 +37,7 @@ module msd_dimm;
           $display("%t \t channel=%d RD0 bankg=%d bank=%d column=%d",$time,q_out_temp[6],q_out_temp[9:7],q_out_temp[11:10],q_out_temp[17:12]);
           $display("%t \t channel=%d RD1 bankg=%d bank=%d column=%d",$time,q_out_temp[6],q_out_temp[9:7],q_out_temp[11:10],q_out_temp[17:12]);
           $display("%t \t channel=%d PRE bankg=%d bank=%d ",$time,q_out_temp[6],q_out_temp[9:7],q_out_temp[11:10]);
-       end 
+       end
        if (q_out_temp[37:36] == 2) begin
           $display("\t\t*********Write operation********* Value of operation=%0d",q_out_temp[37:36]);
           $display("%t \t channel=%d ACT0 bankg=%d bank=%d row =%d",$time,q_out_temp[6],q_out_temp[9:7],q_out_temp[11:10],q_out_temp[33:18]);
@@ -45,7 +45,7 @@ module msd_dimm;
           $display("%t \t channel=%d WR0 bankg=%d bank=%d column=%d",$time,q_out_temp[6],q_out_temp[9:7],q_out_temp[11:10],q_out_temp[17:12]);
           $display("%t \t channel=%d WR1 bankg=%d bank=%d column=%d",$time,q_out_temp[6],q_out_temp[9:7],q_out_temp[11:10],q_out_temp[17:12]);
           $display("%t \t channel=%d REF bankg=%d bank=%d ",$time,q_out_temp[6],q_out_temp[9:7],q_out_temp[11:10]);
-       end 
+       end
   endtask
  
   task rem_from_mc_q;
@@ -54,8 +54,8 @@ module msd_dimm;
        output_command(q_out_temp);
        q_mc.pop_front();
        $display(">>>@time:%t Removing a queue %h elements from queue..  sim_time = %0d --> q_mc.size = %0d\n",$time,q_out_temp, sim_time, q_mc.size());
-       display_q;    
-       last_line ++; 
+       display_q;
+       last_line ++;
   endtask
  
   always@(sim_time) begin
@@ -64,8 +64,8 @@ module msd_dimm;
             wait(sim_t<=sim_time);
             $display("inside simulation time ip =%d, sim_time=%d",sim_t,sim_time);
             add_to_mc_q;
-         end 
-  end 
+         end
+  end
  
   always@(sim_time) begin
          if (q_mc.size() == 15) begin
@@ -78,27 +78,29 @@ module msd_dimm;
             $display("the queue is empty");
          end else
             q_empty=0;
-  end 
+  end
  
-  always@(sim_time) begin 
+  always@(sim_time) begin
          if ((q_mc.size() != 0)||(q_full==1)) begin
             if (sim_time % 2 == 0)
             rem_from_mc_q;
-         end 
-  end 
+         end
+  end
  
   always@(sim_time) begin
-         if ((q_ip_time.size() == 0) && (q_mc.size() ==0) && (q_full ==0) && (last_line !=0)) begin
+         //$display("value of last_line=%d, rowCounter=%d",last_line,rowCounter);
+         if ((q_ip_time.size() == 0) && (q_mc.size() ==0) && (q_full ==0) && (last_line == rowCounter)) begin
+            $display("value of last_line=%d, rowCounter=%d",last_line,rowCounter);
             // Both input queue and memory controller queue are empty
             #1 $finish;
-         end 
-  end 
-     
+         end
+  end
+ 
   task display_q;
        $write("MEMORY_CONTROLLER Q: ");
        for (int i=0; i<q_mc.size(); i++)begin
            $write("%h ", q_mc[i]);
-       end 
+       end
        $write("\n\n");
   endtask
  
@@ -107,7 +109,7 @@ module msd_dimm;
        void'($value$plusargs("ip_file=%s", ip_file));
        void'($value$plusargs("op_file=%s", op_file));
        void'($value$plusargs("debug_en=%d",debug_en));
-  end 
+  end
  
   initial begin
        if (debug_en)
@@ -119,12 +121,12 @@ module msd_dimm;
           if (debug_en)
              $display("Trace file not found. Opening the default trace file 'default_trace.txt' ... ");
           traceFile =$fopen("default_trace.txt","r");
-       end 
+       end
        if (out_file == 0) begin
           if (debug_en)
              $display("output file to print not found. Opening the default output file 'default_out.txt'... ");
           out_file = $fopen("default_output.txt", "w");
-       end 
+       end
        // Read and display values from the file.
        while (!$feof(traceFile)) begin
              // Initialize the values read counter.
@@ -136,19 +138,19 @@ module msd_dimm;
              q_ip_addr.push_back(address);
              //$display("read =%d,write=%d,inst_fetch=%d",read,write,inst_fetch);
              if (valuesRead == 4) begin
-                if (debug_en) begin 
+                if (debug_en) begin
                    $display ( "from row %d the value of time =%d  core=%d operation=%d address=%h",rowCounter,time_unit,core,operation,address);
                    $fwrite(out_file,"from row %d the value of time =%d \t core=%d \t operation=%d \t address=%h \n",rowCounter,time_unit,core,operation,address);
-                end 
+                end
                 rowCounter++;
-             end 
-       end 
+             end
+       end
        // Close the file.
        q_ip_time.pop_back();
        q_ip_oper.pop_back();
        q_ip_addr.pop_back();
        $fclose(traceFile);
        $fclose(out_file);
-  end 
+  end
 endmodule
 
